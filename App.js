fee6cef734b70c1d1738487a1a6b833cd57d569c
guseyn <guseyn@guseyns-MacBook-Pro.local>
2019-03-15 02:35:41 +0400

    main

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { AppRegistry, View, Text } from 'react-native'
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
+import MainActivity from './components/MainActivity'
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#6dc17b',
+    accent: '#ffffff',
+  },
+};
 
 export default class App extends React.Component {
-  render() {
+  render () {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+      <PaperProvider theme={theme}>
+        <MainActivity/>
+      </PaperProvider>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('main', () => App)
